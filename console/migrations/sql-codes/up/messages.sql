14dfe3d9bbc8ae43fb1c036c53622f3e41393dd7
tzendos <hacking.memory@gmail.com>
2017-11-22 22:54:33 +0300

    Изменил связьи в запросах

--- console/migrations/sql-codes/up/messages.sql
+++ console/migrations/sql-codes/up/messages.sql
@@ -1,4 +1,4 @@
-CREATE TABLE IF NOT EXISTS `messages` (
+CREATE TABLE IF NOT EXISTS `mydb`.`messages` (
   `id` INT NOT NULL,
   `chat_id` INT NOT NULL,
   `user_id` INT NOT NULL,
@@ -7,9 +7,15 @@ CREATE TABLE IF NOT EXISTS `messages` (
   `updated_at` DATETIME NOT NULL,
   PRIMARY KEY (`id`),
   INDEX `fk_messages_chats1_idx` (`chat_id` ASC),
+  INDEX `fk_messages_users1_idx` (`user_id` ASC),
   CONSTRAINT `fk_messages_chats1`
     FOREIGN KEY (`chat_id`)
-    REFERENCES `chats` (`id`)
+    REFERENCES `mydb`.`chats` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_messages_users1`
+    FOREIGN KEY (`user_id`)
+    REFERENCES `mydb`.`users` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
--- console/migrations/sql-codes/up/user_chats.sql
+++ console/migrations/sql-codes/up/user_chats.sql
@@ -1,4 +1,4 @@
-CREATE TABLE IF NOT EXISTS `user_chats` (
+CREATE TABLE IF NOT EXISTS `mydb`.`user_chats` (
   `id` INT NOT NULL,
   `user_id` INT NOT NULL,
   `chat_id` INT NOT NULL,
@@ -6,9 +6,15 @@ CREATE TABLE IF NOT EXISTS `user_chats` (
   `updated_at` DATETIME NOT NULL,
   PRIMARY KEY (`id`),
   INDEX `fk_user_chats_chats_idx` (`chat_id` ASC),
+  INDEX `fk_user_chats_users1_idx` (`user_id` ASC),
   CONSTRAINT `fk_user_chats_chats`
     FOREIGN KEY (`chat_id`)
-    REFERENCES `chats` (`id`)
+    REFERENCES `mydb`.`chats` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_user_chats_users1`
+    FOREIGN KEY (`user_id`)
+    REFERENCES `mydb`.`users` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
