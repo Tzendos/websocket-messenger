4db0fa1e77abb834b9949c5aa6890d73aa480048
tzendos <hacking.memory@gmail.com>
2017-11-23 01:53:33 +0300

    Исправлены типы данных в sql скриптах

--- console/migrations/sql-codes/up/messages.sql
+++ console/migrations/sql-codes/up/messages.sql
@@ -1,13 +1,14 @@
 CREATE TABLE IF NOT EXISTS `messages` (
-  `id` INT NOT NULL,
-  `chat_id` INT NOT NULL,
-  `user_id` INT NOT NULL,
-  `to_user_id` INT NULL,
+  `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `chat_id` int(10) UNSIGNED NOT NULL,
+  `user_id` int(10) UNSIGNED NOT NULL,
+  `to_user_id` int(10) UNSIGNED NOT NULL,
   `created_at` DATETIME NOT NULL,
   `updated_at` DATETIME NOT NULL,
   PRIMARY KEY (`id`),
   INDEX `fk_messages_chats1_idx` (`chat_id` ASC),
   INDEX `fk_messages_users1_idx` (`user_id` ASC),
+  INDEX `fk_messages_users2_idx` (`to_user_id` ASC),
   CONSTRAINT `fk_messages_chats1`
     FOREIGN KEY (`chat_id`)
     REFERENCES `chats` (`id`)
@@ -17,4 +18,10 @@ CREATE TABLE IF NOT EXISTS `messages` (
     FOREIGN KEY (`user_id`)
     REFERENCES `users` (`id`)
     ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_messages_users2`
+    FOREIGN KEY (`to_user_id`)
+    REFERENCES `users` (`id`)
+    ON DELETE NO ACTION
     ON UPDATE NO ACTION)
+ENGINE = InnoDB;
--- console/migrations/sql-codes/up/user_chats.sql
+++ console/migrations/sql-codes/up/user_chats.sql
@@ -1,14 +1,20 @@
 CREATE TABLE IF NOT EXISTS `user_chats` (
-  `id` INT NOT NULL,
-  `user_id` INT NOT NULL,
-  `chat_id` INT NOT NULL,
+  `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `user_id` int(10) UNSIGNED NOT NULL,
+  `chat_id` int(10) UNSIGNED NOT NULL,
   `created_at` DATETIME NOT NULL,
   `updated_at` DATETIME NOT NULL,
   PRIMARY KEY (`id`),
   INDEX `fk_user_chats_chats_idx` (`chat_id` ASC),
+  INDEX `fk_user_chats_users1_idx` (`user_id` ASC),
   CONSTRAINT `fk_user_chats_chats`
     FOREIGN KEY (`chat_id`)
     REFERENCES `chats` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_user_chats_users1`
+    FOREIGN KEY (`user_id`)
+    REFERENCES `users` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
