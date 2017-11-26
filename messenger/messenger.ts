4b28d3a5c66e37ec1d2948408ac6e0794dce0a55
tzendos <hacking.memory@gmail.com>
2017-11-27 02:59:45 +0300

    Система срабатывания серверных событий

---
+++ messenger/messenger.ts
@@ -0,0 +1,58 @@
+import * as WebServer from 'ws';
+import * as auth from './auth';
+import {ChatsAction} from "./actions/chats";
+import {ReceiveAction} from "./actions/receive";
+import {MissAction} from "./actions/miss";
+import {SendAction} from "./actions/send";
+import {AuthAction} from "./actions/auth";
+
+export class MessageServer {
+
+    protected wss: WebServer.Server;
+
+    private actions = {};
+
+    async run(port: number) {
+
+        this.wss = new WebServer.Server({port: port});
+        this.registerActions(this.wss);
+
+        this.wss.on('connection', function connection(socket) {
+
+            socket.isAuth = false;
+
+            socket.on('message', function incoming(message: string) {
+                let messageObj = JSON.parse(message);
+
+                if (this.actions[messageObj.typeMessage] != null)
+                    this.actions[messageObj.typeMessage](socket, messageObj);
+                else {
+                    socket.send({'error': 'Неверный тип сообщения'});
+                }
+            }.bind(this));
+        }.bind(this));
+    }
+
+    registerActions(ws) {
+
+        let authAction = new AuthAction(this.wss);
+        this.actions[authAction.getEventType()] = authAction.handle
+            .bind(authAction);
+
+        let chatsAction = new ChatsAction(this.wss);
+        this.actions[chatsAction.getEventType()] = chatsAction.handle
+            .bind(chatsAction);
+
+        let missAction = new MissAction(this.wss);
+        this.actions[missAction.getEventType()] = missAction.handle
+            .bind(missAction);
+
+        let receiveAction = new ReceiveAction(this.wss);
+        this.actions[receiveAction.getEventType()] = receiveAction.handle
+            .bind(receiveAction);
+
+        let sendAction = new SendAction(this.wss);
+        this.actions[sendAction.getEventType()] = sendAction.handle
+            .bind(sendAction);
+    }
+}
